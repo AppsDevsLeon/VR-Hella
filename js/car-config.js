c2f6204de7843e9bccefca6783ac7af64ae31414
MarquezFigueroa <smarkezf@gmail.com>
2025-02-15 11:04:30 -0600

    V2

---
+++ js/car-config.js
@@ -0,0 +1,15 @@
+//Variable json para definir los modelos de los carros
+
+const carModel=[
+   {
+    id:"car1",
+    url:"urlRaw",
+    color:"",
+    nDoors:"",
+    typeCar:"",
+    velocity:"",
+    aceleration:"",
+    description:""
+   }
+
+]
